a8259459b0709ba6df8f4937c3cc3032cc0b54c8
ipe <devnull@localhost>
2010-04-17 18:03:21 +0100

    beginnings of the cli interface

---
+++ openletters/parseText/main.py
@@ -0,0 +1,25 @@
+from parseText import parse_text
+from parseText import parse_date
+
+'''
+Class to parse the Dickens letters and enter into a Redis store
+@author: Iain Emsley (print.crimes@yatterings.com)
+'''
+
+text = open('C:\\Users\\iain\\texts\\dickens\\letterone.txt').read()
+
+#remove the asterisks
+text = text.replace("*","")
+
+#split the body into individual letters
+for letter in text.split("[Sidenote"):
+    m_let = parse_text.parseCorrespondent(letter)
+    #print m_let
+    for l in letter.split("\n"):
+        #print "line is", line
+        m_sal = parse_text.parseSalutation(l)
+        if m_sal != '':
+            '''need to put the line into a list/dictionary'''
+            print m_sal
+        m_date_let = parse_date.parseDate(l)
+        print m_date_let
