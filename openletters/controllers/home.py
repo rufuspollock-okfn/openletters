e9efbff55a187838dd8f555a60d939295ed8ff76
rgrp <http://rufuspollock.org>
2010-04-22 09:40:53 +0100

    [wui][m]: create index page and basic site template (based on openshakespeare).

---
+++ openletters/controllers/home.py
@@ -0,0 +1,16 @@
+import logging
+
+from pylons import request, response, session, tmpl_context as c
+from pylons.controllers.util import abort, redirect_to
+
+from openletters.lib.base import BaseController, render
+
+log = logging.getLogger(__name__)
+
+class HomeController(BaseController):
+    def index(self):
+        return render('home/index.html')
+
+    def gallery(self):
+        return render('home/index.html')
+
