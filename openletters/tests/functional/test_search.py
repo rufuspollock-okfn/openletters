84b194847cb5d1aa40bb4985d5bf14d097fc0e49
iain <devnull@localhost>
2010-10-17 16:00:20 +0100

    search templates and controller

---
+++ openletters/tests/functional/test_search.py
@@ -0,0 +1,8 @@
+from openletters.tests import *
+
+class TestSearchController(TestController):
+
+    def test_index(self):
+        response = self.app.get(url(controller='search', action='index'))
+        assert 'search' in reponse
+        
