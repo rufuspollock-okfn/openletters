46369b68d3ab920f96f97258ca0f2881085d77b9
Andy Lulham <a.lulham@gmail.com>
2013-10-02 02:55:29 +0100

    Add an API.
    Not the most beautiful one, but it sort of works

--- openletters/app.py
+++ openletters/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, app, render_template, request
+import json, urllib2
+
+from flask import Flask, app, render_template, request, url_for
 
 import dao
 
@@ -25,10 +27,14 @@ def author(name=None):
     if name is None:
         name = dao.getdataindex()
 
+        if request.args.get('json', None) is not None:
+            return json.dumps([{'name': x, 'path': url_for('author', name=x)} for x in sorted(name)])
         return render_template('authorindex.html', name=sorted(name))
     else:
-        name = name.replace('%20', ' ')
+        name = urllib2.unquote(name)
         dates = dao.getdataauthor(name)
+        if request.args.get('json', None) is not None:
+            return json.dumps([{'date': date, 'path': url_for('letter', letterid=id)} for id, date in dates.iteritems()])
         return render_template('author.html', dates=dates, name=name)        
     
 @app.route('/letter/<int:letterid>', methods=['GET'])
@@ -38,6 +44,8 @@ def letter(letterid=None):
         return render_template('letterindex.html')
     else:
         letter = dao.getletter(letterid)
+        if request.args.get('json', None) is not None:
+            return json.dumps(letter)
         return render_template('letter.html', letter=letter) 
     
   
@@ -48,11 +56,15 @@ def place(location=None):
     if location is None:
         location = dao.getplaceindex()
         latlon = dao.getplacemap()
-        print latlon
+        if request.args.get('json', None) is not None:
+            return json.dumps([{'place': x, 'path': url_for('place', location=x)} for x in sorted(location)])
         return render_template('placemap.html', location=sorted(location))
         #return render_template('placemap.html', place=location, latlon=latlon)
     else:
         letter = dao.getplace(location)
+        if request.args.get('json', None) is not None:
+            return json.dumps([dict({'path': url_for('letter', letterid=k)}.items() + v.items()) for k,v in letter.iteritems()])
         return render_template('placeindex.html', place=letter, location=location)  
+
 if __name__ == '__main__':
     app.run(debug=True)
